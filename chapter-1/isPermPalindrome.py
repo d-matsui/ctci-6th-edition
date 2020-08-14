ee7b4f59ecedc9dfc1de01b3711d361ce1556281
Daiki Matsui <mti.daiki@gmail.com>
2020-08-14 18:00:36 +0900

    refactor and optimize code

---
+++ chapter-1/isPermPalindrome.py
@@ -0,0 +1,52 @@
+import pytest
+from collections import defaultdict
+
+
+# O(N)
+def is_perm_palindrome(string):
+    # check if a string is a permutation of a palindrome or not
+    nospace_lower_string = string.replace(' ', '').lower()
+    table = build_char_frequency_table(nospace_lower_string)
+    return check_max_one_odd(table)
+
+
+def build_char_frequency_table(string):
+    char_frequency_table = defaultdict(int)
+
+    for char in string:
+        char_frequency_table[char] += 1
+
+    return char_frequency_table
+
+
+def check_max_one_odd(table):
+    num_odds = 0
+    for char_frequency in table.values():
+        if char_frequency % 2 == 1:
+            num_odds += 1
+            if num_odds > 1:
+                return False
+
+    return True
+
+
+# optimization
+def check_max_one_odd_optimized(string):
+    char_frequency_table = defaultdict(int)
+    num_odds = 0
+
+    for char in string:
+        char_frequency_table[char] += 1
+        if char_frequency_table[char] % 2 == 1:
+            num_odds += 1
+        else:
+            num_odds -= 1
+
+    return num_odds <= 1
+
+
+def test_is_perm_palindrome():
+    assert is_perm_palindrome('tact coa') == True
+    assert is_perm_palindrome('tAc tca') == True
+    assert is_perm_palindrome('tact boa') == False
+    assert is_perm_palindrome('tac bca') == False
